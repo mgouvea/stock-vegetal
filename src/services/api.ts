6a03820a32c5de3e3c24ec11d6f7aa9f326507af
Mateus <mateuscg42@gmail.com>
2022-03-05 17:54:33 -0300

    fix axios baseURL api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
+let baseURL = '';
+
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3000/api';
+} else {
+  baseURL = 'https://sala-vegetal.vercel.app/api';
+}
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: baseURL,
 });
